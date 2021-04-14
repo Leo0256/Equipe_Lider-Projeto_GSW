77693c774e70bfd528489261b7d4e26c63c794af
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2021-04-14 14:35:21 -0300

    atualização

--- Arquivos/Códigos/GSWSystem/GSWSystem/MainWindow.xaml.cs
+++ Arquivos/Códigos/GSWSystem/GSWSystem/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace GSWSystem
 {
     /// <summary>
@@ -24,5 +25,6 @@ namespace GSWSystem
         {
             InitializeComponent();
         }
+
     }
 }
