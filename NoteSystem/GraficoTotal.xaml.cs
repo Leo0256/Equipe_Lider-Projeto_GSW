22267abb986de0acc960875baaffdb65addac7a7
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2021-06-04 13:48:37 -0300

    Implementação

---
+++ NoteSystem/GraficoTotal.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace NoteSystem
+{
+    /// <summary>
+    /// Interação lógica para TotalStatus.xam
+    /// </summary>
+    public partial class GraficoTotal : Page
+    {
+        public GraficoTotal(bool type, string[] param1, string[] param2)
+        {
+            InitializeComponent();
+
+            if (type)
+            {
+                Nome.Content = "Status";
+            }
+            else
+            {
+                Nome.Content = "Projeto";
+            }
+
+            for(int i = 0; i < param1.Length; i++)
+            {
+                defineGrid(param1[i], param2[i], i);
+                Divisor.Height += 110;
+            }
+        }
+
+        private void defineGrid(string status, string total, int i)
+        {
+            RowDefinition row = new();
+            DataGrid.RowDefinitions.Add(row);
+            DataGrid.RowDefinitions[i].Height = new GridLength(120);
+
+            Label lbl1 = setData(status);
+            Grid.SetColumn(lbl1, 0);
+            Grid.SetRow(lbl1, i);
+
+            Label lbl2 = setData(total);
+            Grid.SetColumn(lbl2, 1);
+            Grid.SetRow(lbl2, i);
+
+            DataGrid.Children.Add(lbl1);
+            DataGrid.Children.Add(lbl2);
+        }
+
+        private Label setData(string str)
+        {
+            Label lbl = new();
+
+            lbl.Content = str;
+            lbl.MaxWidth = 390;
+            lbl.FontFamily = new FontFamily("Arial");
+            lbl.FontSize = 26;
+            lbl.Foreground = new SolidColorBrush(Color.FromRgb(21, 21, 21));
+            lbl.HorizontalAlignment = HorizontalAlignment.Left;
+            lbl.VerticalContentAlignment = VerticalAlignment.Center;
+
+            return lbl;
+        }
+    }
+}
---
+++ NoteSystem/QuantHoras.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace NoteSystem
+{
+    /// <summary>
+    /// Interação lógica para QuantHoras.xam
+    /// </summary>
+    public partial class QuantHoras : Page
+    {
+        private string tag;
+        public QuantHoras(string Type, string xNome, string xHoras, string comp)
+        {
+            InitializeComponent();
+            tag = string.Empty;
+            switch (Type)
+            {
+                case "ano":
+                    tag = "Ano: " + xNome;
+                    break;
+
+                case "mes":
+                    var mes = DateTimeFormatInfo.CurrentInfo.GetMonthName(int.Parse(xNome));
+                    tag = char.ToUpper(mes[0]) + mes[1..] + " / " + comp;
+                    break;
+
+                default:
+                    tag = xNome;
+                    break;
+
+            }
+            
+            Nome.Text = tag;
+            Horas.Content = "Horas: " + xHoras.Replace(",", ":");
+        }
+    }
+}
